8134f8246c4354aaffa5339df0b2c5c7ab21a063
Mukul-bhumiitech091 <mukul.kumar@bhumiitech.com>
2023-08-18 10:34:27 +0530

    single file uploader

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Uploader } from "./Components/uploader";
+import { MultiFileUploader } from "./Components/multiFileUploader";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Uploader /> */}
+      <MultiFileUploader />
+    </div>
+  );
+}
+
+export default App;
